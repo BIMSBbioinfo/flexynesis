2645918f5aeeb764bafd85dd25bf252e2d86af99
borauyar <bora.uyar@mdc-berlin.de>
2023-03-16 16:09:16 +0100

    add encoder/decoder models

--- flexynesis/models_shared.py
+++ flexynesis/models_shared.py
@@ -3,6 +3,29 @@
 from torch import nn
 
 
+class Encoder(nn.Module):
+    def __init__(self, input_dim, hidden_dim, latent_dim):
+        super(Encoder, self).__init__()
+        self.FC_input = nn.Linear(input_dim, hidden_dim)
+        self.FC_mean  = nn.Linear(hidden_dim, latent_dim)
+        self.FC_var   = nn.Linear (hidden_dim, latent_dim)
+        self.LeakyReLU = nn.LeakyReLU(0.2)
+    def forward(self, x):
+        h_       = self.LeakyReLU(self.FC_input(x))
+        mean     = self.FC_mean(h_)
+        log_var  = self.FC_var(h_)                     
+        return mean, log_var
+
+class Decoder(nn.Module):
+    def __init__(self, latent_dim, hidden_dim, output_dim):
+        super(Decoder, self).__init__()
+        self.FC_hidden = nn.Linear(latent_dim, hidden_dim)
+        self.FC_output = nn.Linear(hidden_dim, output_dim)
+        self.LeakyReLU = nn.LeakyReLU(0.2)
+    def forward(self, x):
+        h     = self.LeakyReLU(self.FC_hidden(x))
+        x_hat = torch.sigmoid(self.FC_output(h))
+        return x_hat
 
 # a MLP model for regression/classification
 # set num_class to 1 for regression. num_class > 1 => classification
