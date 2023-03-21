b14becd20e76e01b7098111a091ee667c191d19a
borauyar <bora.uyar@mdc-berlin.de>
2023-03-21 12:13:03 +0100

    integrate feature selection using laplacian scores into the vignettes

--- vignettes/DirectPred.py
+++ vignettes/DirectPred.py
@@ -6,6 +6,19 @@ import os
 import torch
 
 
+# filter features to keep top 20% based on laplacian score, keeping minimum of 1000 features
+# TODO: make this configurable
+def filter(dat):
+    counts = {x: max(int(dat[x].shape[0]/20), 1000) for x in dat.keys()}
+    dat = {x: flexynesis.filter_by_laplacian(dat[x].T, topN=counts[x]).T for x in dat.keys()}
+    return dat
+
+# subset dat2 to only include features available in dat1
+def harmonize(dat1, dat2):
+    # features to keep
+    features = {x: dat1[x].index for x in dat1.keys()}
+    return {x: dat2[x].loc[features[x]] for x in dat2.keys()}
+
 if __name__ == '__main__':
     # get data
     # output options
@@ -26,6 +39,9 @@ if __name__ == '__main__':
     drugs = pd.concat([pd.read_csv(os.path.join(inputDir, 'train', 'clin.csv'), sep = '\t').transpose(),
                       pd.read_csv(os.path.join(inputDir, 'test', 'clin.csv'), sep = '\t').transpose()])
 
+    dat_train = filter(dat_train)
+    dat_holdout = harmonize(dat_train, dat_holdout)
+
     # Set concatenate to True to use early fusion, otherwise it will run intermediate fusion
     train_dataset = flexynesis.data.make_dataset(dat_train, drugs, drugName, batch_size, concatenate = False)
     holdout_dataset = flexynesis.data.make_dataset(dat_holdout, drugs, drugName, batch_size, concatenate = False)
@@ -42,7 +58,7 @@ if __name__ == '__main__':
     stats = pd.DataFrame.from_dict({'RMSE': 'NA', 'Rsquare': 'NA', 'COR': COR, 
                                         'drug': drugName, 'trainSampleN': len(train_dataset), 
                                         'testSampleN': len(holdout_dataset), 
-                                        'tool': 'limo'}, orient = 'index').T
+                                        'tool': 'DirectPred'}, orient = 'index').T
     
     # save stats 
     outFile = os.path.join(outDir,  '.'.join(['stats', drugName, 'tsv']))
--- vignettes/svae.py
+++ vignettes/svae.py
@@ -5,6 +5,19 @@ import pandas as pd
 import os
 import torch
 
+# filter features to keep top 20% based on laplacian score, keeping minimum of 1000 features
+# TODO: make this configurable
+def filter(dat):
+    counts = {x: max(int(dat[x].shape[0]/20), 1000) for x in dat.keys()}
+    dat = {x: flexynesis.filter_by_laplacian(dat[x].T, topN=counts[x]).T for x in dat.keys()}
+    return dat
+
+# subset dat2 to only include features available in dat1
+def harmonize(dat1, dat2):
+    # features to keep
+    features = {x: dat1[x].index for x in dat1.keys()}
+    return {x: dat2[x].loc[features[x]] for x in dat2.keys()}
+    
 
 if __name__ == '__main__':
     # get data
@@ -27,18 +40,22 @@ if __name__ == '__main__':
     drugs = pd.concat([pd.read_csv(os.path.join(inputDir, 'train', 'clin.csv'), sep = '\t').transpose(),
                       pd.read_csv(os.path.join(inputDir, 'test', 'clin.csv'), sep = '\t').transpose()])
 
+    
+    dat_train = filter(dat_train)
+    dat_holdout = harmonize(dat_train, dat_holdout)
+    
     # Set concatenate to True to use early fusion, otherwise it will run intermediate fusion
-    train_dataset = flexynesis.data.make_dataset(dat_train, drugs, drugName, batch_size, concatenate = False)
-    holdout_dataset = flexynesis.data.make_dataset(dat_holdout, drugs, drugName, batch_size, concatenate = False)
+    train_dataset = flexynesis.make_dataset(dat_train, drugs, drugName, batch_size, concatenate = False)
+    holdout_dataset = flexynesis.make_dataset(dat_holdout, drugs, drugName, batch_size, concatenate = False)
     
     # define model 
     layers = list(train_dataset.dat.keys())
     input_dims = [len(train_dataset.features[layers[i]]) for i in range(len(layers))] # number of features per layer
-    model = flexynesis.supervised_vae_if(num_layers = len(layers),
-                                         input_dims = input_dims, 
-                                         hidden_dims = hidden_dims, 
-                                         latent_dim = latent_dim, 
-                                         num_class = 1, h = 8)
+    model = flexynesis.supervised_vae(num_layers = len(layers),
+                                     input_dims = input_dims, 
+                                     hidden_dims = hidden_dims, 
+                                     latent_dim = latent_dim, 
+                                     num_class = 1, h = 8)
     # train model
     model = flexynesis.train_model(model, train_dataset, n_epoch, batch_size, val_size = 0.2) 
 
