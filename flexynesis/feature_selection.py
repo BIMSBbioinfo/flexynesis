5df8b18696dc0f6e028703156057e4b7a9351598
borauyar <bora.uyar@mdc-berlin.de>
2023-03-21 11:27:43 +0100

    write functions to use laplacian scores and remove highly correlated features

--- flexynesis/feature_selection.py
+++ flexynesis/feature_selection.py
@@ -10,7 +10,7 @@ def laplacian_score(X, k=5, t=None):
     Calculate Laplacian Score for each feature in the dataset.
     
     Parameters:
-    X: (n_samples, n_features) - Input data
+    X: numpy array (n_samples, n_features) - Input data
     k: int - Number of nearest neighbors to consider
     t: float - Heat kernel parameter (optional)
     
@@ -40,11 +40,86 @@ def laplacian_score(X, k=5, t=None):
     # Calculate Laplacian Score for each feature
     scores = []
     for i in range(n_features):
-        fi = X[X.columns[i]]
+        fi = X[:,i]
         fi = fi - np.dot(S, fi).sum() / n_samples
         L_score = np.dot(fi.T, np.dot(L, fi)) / np.dot(fi.T, np.dot(D, fi))
         scores.append(L_score)
 
     return np.array(scores)
 
+def remove_redundant_features(X, laplacian_scores, threshold=0.8):
+    """
+    Efficiently selects features based on Laplacian scores while avoiding highly correlated features.
 
+    Parameters
+    ----------
+    X : numpy array, shape (n_samples, n_features)
+        The input data matrix with samples in rows and features in columns.
+    laplacian_scores : numpy array, shape (n_features,)
+        The Laplacian scores for each feature in the input data matrix X.
+    threshold : float, optional, default: 0.8
+        The Pearson correlation coefficient threshold for removing highly correlated features.
+        Features with correlation coefficients greater than the threshold will be removed.
+
+    Returns
+    -------
+    selected_features : list of int
+        The list of selected feature indices in the input data matrix X.
+    """
+
+    # Compute the correlation matrix once
+    correlation_matrix = np.corrcoef(X, rowvar=False)
+    
+    # Rank the features by Laplacian scores
+    ranked_indices = np.argsort(laplacian_scores)
+
+    selected_features = []
+
+    for idx in ranked_indices:
+        correlated = False
+
+        for selected_idx in selected_features:
+            # Check the absolute correlation between the features
+            correlation = np.abs(correlation_matrix[idx, selected_idx])
+
+            if correlation > threshold:
+                correlated = True
+                break
+
+        if not correlated:
+            selected_features.append(idx)
+
+    return selected_features
+
+def filter_by_laplacian(X, k=5, t=None, topN = 100, remove_redundant = True, threshold = 0.8):
+    """
+    Given a data matrix, compute laplacian score for each feature
+    and return a filtered data matrix based on top laplacian scores.
+    
+    Parameters:
+    X: Pandas Data Frame (n_samples, n_features) - Input data
+    k: int - Number of nearest neighbors to consider
+    t: float - Heat kernel parameter (optional)
+    topN = Top number of features to keep
+    remove_redundant = (Boolean) whether to remove highly correlated 
+        features based on laplacian score ranking. Default: True. 
+    Returns:
+    X_selected: (n_samples, n_features) - Filtered data matrix
+    """
+    # compute laplacian scores
+    scores = laplacian_score(X.values, k, t)
+    
+    # Sort the features based on their Laplacian Scores
+    sorted_indices = np.argsort(scores)
+
+    if remove_redundant:
+        # sorted indices filtered for correlation
+        sorted_indices = remove_redundant_features(X.values, scores, threshold)
+        
+    # Choose the top k features with the lowest Laplacian Scores
+    selected_features = sorted_indices[:topN]
+
+    # Extract the selected features from the dataset
+    X_selected = X[X.columns[selected_features]]
+    
+    return X_selected
