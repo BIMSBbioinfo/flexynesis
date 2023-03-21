a83e8d2650ce4c72492e6074047601cd7ea80f1e
borauyar <bora.uyar@mdc-berlin.de>
2023-03-21 10:28:10 +0100

    replace LIMO with DirectPred, which is a flexible architecture in terms of number of omics layers eusing FCNs to directly predict outcome varible

--- flexynesis/model_DirectPred.py
+++ flexynesis/model_DirectPred.py
@@ -16,31 +16,34 @@ from functools import reduce
 
 from .models_shared import *
     
-class LIMO(pl.LightningModule):
-    def __init__(self, num_feature1, num_feature2, embedding_size = 16, num_class = 1, **kwargs):
-        super(LIMO, self).__init__()
-        self.omics1 = MLP(num_feature1, embedding_size, **kwargs)
-        self.omics2 = MLP(num_feature2, embedding_size, **kwargs)
-        self.MLP = MLP(num_feature = embedding_size * 2, num_class = num_class, h = 64)
-        self.embedding_size = embedding_size
+# num_layers: number of omics layers/matrices 
+class DirectPred(pl.LightningModule):
+    def __init__(self, num_layers, input_dims, latent_dim = 16, num_class = 1, **kwargs):
+        super(DirectPred, self).__init__()
+         # create a list of Encoder instances for separately encoding each omics layer
+        self.encoders = nn.ModuleList([MLP(input_dims[i], latent_dim, **kwargs) for i in range(num_layers)])
+        # fusion layer
+        self.MLP = MLP(num_feature = latent_dim * num_layers, num_class = num_class, h = 64)
+        self.latent_dim = latent_dim
         
-    def forward(self, dat_omics1, dat_omics2):
-        l1 = self.omics1(dat_omics1) #embeddings from layer1 (e.g. gex)
-        l2 = self.omics2(dat_omics2) #embeddings from layer2 (e.g. cnv)
-        #run the regressor on concatenated encodings 
-        l = torch.cat((l1, l2), dim = 1)
-        y_pred = self.MLP(l)
-        return y_pred, l1, l2
+    def forward(self, x_list):
+        embeddings_list = []
+        # Process each input matrix with its corresponding Encoder
+        for i, x in enumerate(x_list):
+            embeddings_list.append(self.encoders[i](x))
+        # Push concatenated encodings through a fully connected network to predict
+        y_pred = self.MLP(torch.cat(embeddings_list, dim=1))
+        return y_pred, embeddings_list
     
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=0.004048966088420985)
+        optimizer = torch.optim.Adam(self.parameters(), lr=0.001)
         return optimizer
 
     def training_step(self, train_batch, batch_idx):
         dat, y = train_batch
-        omics = list(dat.keys())
-        y_hat = self.forward(dat[omics[0]], 
-                             dat[omics[1]])[0]
+        layers = dat.keys()
+        x_list = [dat[x] for x in layers]
+        y_hat = self.forward(x_list)[0]
         loss = F.mse_loss(torch.flatten(y_hat), y)
         r_value = stats.linregress(y.detach().numpy(), torch.flatten(y_hat).detach().numpy())[2]
         self.log_dict({"train_loss": loss, "train_corr": r_value})
@@ -48,18 +51,18 @@ class LIMO(pl.LightningModule):
     
     def validation_step(self, val_batch, batch_idx):
         dat, y = val_batch
-        omics = list(dat.keys())
-        y_hat = self.forward(dat[omics[0]], 
-                                     dat[omics[1]])[0]
+        layers = dat.keys()
+        x_list = [dat[x] for x in layers]
+        y_hat = self.forward(x_list)[0]
         loss = F.mse_loss(torch.flatten(y_hat), y)
         r_value = stats.linregress(y.detach().numpy(), torch.flatten(y_hat).detach().numpy())[2]
         self.log_dict({"val_loss": loss, "val_corr": r_value})
     
     def evaluate(self, dataset):
         self.eval()
-        omics = list(dataset.dat.keys())
-        y_hat = self.forward(dataset.dat[omics[0]], 
-                             dataset.dat[omics[1]])[0]
+        layers = dataset.dat.keys()
+        x_list = [dataset.dat[x] for x in layers]
+        y_hat = self.forward(x_list)[0]
         r_value = stats.linregress(dataset.y.detach().numpy(),
                                    torch.flatten(y_hat).detach().numpy())[2]
         return r_value
--- vignettes/DirectPred.py
+++ vignettes/DirectPred.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     inputDir = '/data/local/buyar/arcas/multiomics_integration/benchmarks/pharmacogx/output/gdsc2_vs_ccle_gex_cnv/100'
     outDir = '.'
     n_epoch = 200
-    embedding_size = 64
+    latent_dim = 16
     batch_size = 128
     datatypes = ['layer1', 'layer2']
     drugName = 'Erlotinib'
@@ -26,20 +26,21 @@ if __name__ == '__main__':
     drugs = pd.concat([pd.read_csv(os.path.join(inputDir, 'train', 'clin.csv'), sep = '\t').transpose(),
                       pd.read_csv(os.path.join(inputDir, 'test', 'clin.csv'), sep = '\t').transpose()])
 
-
-    train_dataset = flexynesis.data.make_dataset(dat_train, drugs, drugName, batch_size)
-    holdout_dataset = flexynesis.data.make_dataset(dat_holdout, drugs, drugName, batch_size)
+    # Set concatenate to True to use early fusion, otherwise it will run intermediate fusion
+    train_dataset = flexynesis.data.make_dataset(dat_train, drugs, drugName, batch_size, concatenate = False)
+    holdout_dataset = flexynesis.data.make_dataset(dat_holdout, drugs, drugName, batch_size, concatenate = False)
     
-    trainSampleN = len(train_dataset)
-    f1, f2 = [train_dataset.dat[omics].shape[1] for omics in train_dataset.dat.keys()]
-    model = flexynesis.LIMO(f1, f2, h = 128, embedding_size=embedding_size, num_class = 1)
+    layers = list(train_dataset.dat.keys())
+    input_dims = [len(train_dataset.features[layers[i]]) for i in range(len(layers))] # number of features per layer    
+    model = flexynesis.DirectPred(num_layers = len(layers), input_dims = input_dims, 
+                                   latent_dim = latent_dim, num_class = 1, h = 128)
     
     model = flexynesis.main.train_model(model, train_dataset, n_epoch, batch_size, val_size = 0.2)
     
     # evaluate the model on holdout dataset
     COR = model.evaluate(holdout_dataset)
     stats = pd.DataFrame.from_dict({'RMSE': 'NA', 'Rsquare': 'NA', 'COR': COR, 
-                                        'drug': drugName, 'trainSampleN': trainSampleN, 
+                                        'drug': drugName, 'trainSampleN': len(train_dataset), 
                                         'testSampleN': len(holdout_dataset), 
                                         'tool': 'limo'}, orient = 'index').T
     
