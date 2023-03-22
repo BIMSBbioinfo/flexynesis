c04cb6c2fc66c28174d2e73526de3c8ea5970f86
borauyar <bora.uyar@mdc-berlin.de>
2023-03-22 16:02:31 +0100

    change hyperparameters for triplet encoder

--- vignettes/triplet_encoder.py
+++ vignettes/triplet_encoder.py
@@ -9,7 +9,7 @@ import numpy as np
 # filter features to keep top 20% based on laplacian score, keeping minimum of 1000 features
 # TODO: make this configurable
 def filter(dat):
-    counts = {x: max(int(dat[x].shape[0]/20), 1000) for x in dat.keys()}
+    counts = {x: max(int(dat[x].shape[0]/20), 500) for x in dat.keys()}
     dat = {x: flexynesis.filter_by_laplacian(dat[x].T, topN=counts[x]).T for x in dat.keys()}
     return dat
 
@@ -32,8 +32,8 @@ if __name__ == '__main__':
     # output options
     inputDir = '/data/local/buyar/arcas/multiomics_integration/benchmarks/pharmacogx/output/gdsc2_vs_ccle_gex_cnv/100'
     outDir = '.'
-    n_epoch = 300
-    batch_size = 64
+    n_epoch = 200
+    batch_size = 128
     datatypes = ['layer1', 'layer2']
     drugName = 'Erlotinib'
     torch.set_num_threads(4)
@@ -47,8 +47,8 @@ if __name__ == '__main__':
                       pd.read_csv(os.path.join(inputDir, 'test', 'clin.csv'), sep = '\t').transpose()])
 
     # skip filtering
-    #dat_train = filter(dat_train)
-    #dat_holdout = harmonize(dat_train, dat_holdout)
+    dat_train = filter(dat_train)
+    dat_holdout = harmonize(dat_train, dat_holdout)
     
     # Set concatenate to True to use early fusion, otherwise it will run intermediate fusion
     train_dataset = flexynesis.make_dataset(dat_train, drugs, drugName, batch_size, concatenate = False)
@@ -65,15 +65,15 @@ if __name__ == '__main__':
     # define triplet network
     layers = list(train_dataset.dat.keys())
     input_dims = [len(train_dataset.features[layers[i]]) for i in range(len(layers))]
-    hidden_dims = [256, 256]
-    output_dim = 32
+    hidden_dims = [128, 32]
+    output_dim = 16
     # define model 
     multi_triplet_network = flexynesis.MultiTripletNetwork(num_layers = len(layers), input_sizes = input_dims, 
                                                            hidden_sizes = hidden_dims, 
                                                            output_size = output_dim, 
                                                            num_classes=len(triplet_train_dataset.labels_set))    
     # train model
-    model = flexynesis.train_model(multi_triplet_network, triplet_train_dataset, n_epoch, batch_size, val_size = 0.2) 
+    model = flexynesis.train_model(multi_triplet_network, triplet_train_dataset, n_epoch, batch_size, val_size = 0.1) 
 
     z_train, y_pred_train = model.transform(train_dataset)
     z_holdout, y_pred_holdout = model.transform(holdout_dataset)
