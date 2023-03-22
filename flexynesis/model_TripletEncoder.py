d5265924b82f44009878ecd4da3afca3b66cfebf
borauyar <bora.uyar@mdc-berlin.de>
2023-03-22 13:12:24 +0100

    Add MultiOmic Triplet Network with a Classifier head

---
+++ flexynesis/model_TripletEncoder.py
@@ -0,0 +1,82 @@
+# Generating encodings of multi-omic data using triplet loss 
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+import pandas as pd
+import numpy as np
+
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import TQDMProgressBar
+from pytorch_lightning.strategies import DDPStrategy
+
+from .models_shared import *
+
+
+class MultiEmbeddingNetwork(nn.Module):
+    def __init__(self, input_sizes, hidden_sizes, output_size):
+        super(MultiEmbeddingNetwork, self).__init__()
+        self.embedding_networks = nn.ModuleList([
+            EmbeddingNetwork(input_size, hidden_size, output_size)
+            for input_size, hidden_size in zip(input_sizes, hidden_sizes)
+        ])
+
+    def forward(self, x):
+        embeddings = [
+            embedding_network(x[key])
+            for key, embedding_network in zip(x.keys(), self.embedding_networks)
+        ]
+        fused_embedding = torch.cat((embeddings), dim=-1)
+        return fused_embedding
+
+class MultiTripletNetwork(pl.LightningModule):
+    def __init__(self, num_layers, input_sizes, hidden_sizes, output_size, num_classes):
+        super(MultiTripletNetwork, self).__init__()
+        self.multi_embedding_network = MultiEmbeddingNetwork(input_sizes, hidden_sizes, output_size)
+        self.classifier = Classifier(output_size * num_layers, [64], num_classes)
+
+    def forward(self, anchor, positive, negative):
+        anchor_embedding = self.multi_embedding_network(anchor)
+        positive_embedding = self.multi_embedding_network(positive)
+        negative_embedding = self.multi_embedding_network(negative)
+        y_pred = self.classifier(anchor_embedding)
+        return anchor_embedding, positive_embedding, negative_embedding, y_pred
+    
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        return optimizer
+    
+    def triplet_loss(self, anchor, positive, negative, margin=1.0):
+        distance_positive = (anchor - positive).pow(2).sum(1)
+        distance_negative = (anchor - negative).pow(2).sum(1)
+        losses = torch.relu(distance_positive - distance_negative + margin)
+        return losses.mean()    
+
+    def training_step(self, train_batch, batch_idx):
+        anchor, positive, negative, y = train_batch[0], train_batch[1], train_batch[2], train_batch[3]
+        anchor_embedding, positive_embedding, negative_embedding, y_pred = self.forward(anchor, positive, negative)
+        triplet_loss = self.triplet_loss(anchor_embedding, positive_embedding, negative_embedding)
+        # compute classifier loss 
+        cl_loss = F.cross_entropy(y_pred, y)
+        loss = triplet_loss + cl_loss 
+        self.log('train_loss', loss)
+        return loss
+    
+    def validation_step(self, val_batch, batch_idx):
+        anchor, positive, negative, y = val_batch[0], val_batch[1], val_batch[2], val_batch[3]
+        anchor_embedding, positive_embedding, negative_embedding, y_pred = self.forward(anchor, positive, negative)
+        triplet_loss = self.triplet_loss(anchor_embedding, positive_embedding, negative_embedding)
+        y_pred = self.classifier(anchor_embedding)
+        cl_loss = F.cross_entropy(y_pred, y)
+        loss = triplet_loss + cl_loss 
+        self.log('val_loss', loss)
+        return loss
+    
+    # dataset: MultiOmicDataset
+    def transform(self, dataset):
+        self.eval()
+        z = pd.DataFrame(self.multi_embedding_network(dataset.dat).detach().numpy())
+        z.columns = [''.join(['LF', str(x+1)]) for x in z.columns]
+        z.index = dataset.samples
+        return z
+
---
+++ vignettes/triplet_encoder.py
@@ -0,0 +1,88 @@
+# Early fusion Supervised VAE example run
+
+import flexynesis
+import pandas as pd
+import os
+import torch
+import numpy as np
+
+# filter features to keep top 20% based on laplacian score, keeping minimum of 1000 features
+# TODO: make this configurable
+def filter(dat):
+    counts = {x: max(int(dat[x].shape[0]/20), 1000) for x in dat.keys()}
+    dat = {x: flexynesis.filter_by_laplacian(dat[x].T, topN=counts[x]).T for x in dat.keys()}
+    return dat
+
+# subset dat2 to only include features available in dat1
+def harmonize(dat1, dat2):
+    # features to keep
+    features = {x: dat1[x].index for x in dat1.keys()}
+    return {x: dat2[x].loc[features[x]] for x in dat2.keys()}
+
+# label numeric values by position with the median
+def label_by_median(arr):
+    arr = np.asarray(arr)
+    median = np.median(arr)
+    categories = np.where(arr > median, 0, 1) 
+    return categories
+
+
+if __name__ == '__main__':
+    # get data
+    # output options
+    inputDir = '/data/local/buyar/arcas/multiomics_integration/benchmarks/pharmacogx/output/gdsc2_vs_ccle_gex_cnv/100'
+    outDir = '.'
+    n_epoch = 200
+    batch_size = 128
+    datatypes = ['layer1', 'layer2']
+    drugName = 'Erlotinib'
+    torch.set_num_threads(4)
+
+    # import assays and labels
+    dat_train = {x: pd.read_csv(os.path.join(inputDir, 'train', ''.join([x, '.csv']))) for x in datatypes}
+    dat_holdout = {x: pd.read_csv(os.path.join(inputDir, 'test', ''.join([x, '.holdout.csv']))) for x in datatypes}
+
+    # get drug response data (concatenate to keep all labels in one df)
+    drugs = pd.concat([pd.read_csv(os.path.join(inputDir, 'train', 'clin.csv'), sep = '\t').transpose(),
+                      pd.read_csv(os.path.join(inputDir, 'test', 'clin.csv'), sep = '\t').transpose()])
+
+    # skip filtering
+    #dat_train = filter(dat_train)
+    #dat_holdout = harmonize(dat_train, dat_holdout)
+    
+    # Set concatenate to True to use early fusion, otherwise it will run intermediate fusion
+    train_dataset = flexynesis.make_dataset(dat_train, drugs, drugName, batch_size, concatenate = False)
+    holdout_dataset = flexynesis.make_dataset(dat_holdout, drugs, drugName, batch_size, concatenate = False)
+    # convert numeric values to categorical 
+    train_dataset.y = torch.tensor(label_by_median(train_dataset.y))
+    holdout_dataset.y = torch.tensor(label_by_median(holdout_dataset.y))
+    
+    # create triplet datasets
+    # get triplets
+    triplet_train_dataset = flexynesis.TripletMultiOmicDataset(train_dataset)
+    triplet_holdout_dataset = flexynesis.TripletMultiOmicDataset(holdout_dataset, train=False)
+    
+    # define triplet network
+    layers = list(train_dataset.dat.keys())
+    input_dims = [len(train_dataset.features[layers[i]]) for i in range(len(layers))]
+    hidden_dims = [256, 64]
+    output_dim = 16
+    # define model 
+    multi_triplet_network = flexynesis.MultiTripletNetwork(num_layers = len(layers), input_sizes = input_dims, 
+                                                           hidden_sizes = hidden_dims, 
+                                                           output_size = output_dim, 
+                                                           num_classes=len(triplet_train_dataset.labels_set))    
+    # train model
+    model = flexynesis.train_model(multi_triplet_network, triplet_train_dataset, n_epoch, batch_size, val_size = 0.2) 
+
+    z_train = model.transform(train_dataset)
+    z_holdout = model.transform(holdout_dataset)
+    
+    z_train['y'] = train_dataset.y
+    z_holdout['y'] = holdout_dataset.y
+    
+    z_train.to_csv('z_train.csv')
+    z_holdout.to_csv('z_holdout.csv')
+    
+    
+    
