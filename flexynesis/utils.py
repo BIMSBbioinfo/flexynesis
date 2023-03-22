edf8e076ed86cc00a1314e3b0fbc1f1522ee09a2
borauyar <bora.uyar@mdc-berlin.de>
2023-03-22 13:35:41 +0100

    add function to evaluate classificaiton results

--- flexynesis/utils.py
+++ flexynesis/utils.py
@@ -3,6 +3,8 @@ import umap
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+from sklearn.metrics import balanced_accuracy_score, f1_score, cohen_kappa_score, classification_report
+
 
 def plot_umap_scatter(df, n_neighbors=15, min_dist=0.1, n_components=2, metric='euclidean'):
     """
@@ -35,4 +37,21 @@ def plot_umap_scatter(df, n_neighbors=15, min_dist=0.1, n_components=2, metric='
     plt.ylabel('UMAP Component 2')
     plt.title('UMAP Scatter Plot')
     plt.show()
-    
+
+def evaluate_classifier(y_true, y_pred):
+    # Balanced accuracy
+    balanced_acc = balanced_accuracy_score(y_true, y_pred)
+    print(f"Balanced Accuracy: {balanced_acc:.4f}")
+
+    # F1 score (macro)
+    f1 = f1_score(y_true, y_pred, average='macro')
+    print(f"F1 Score (Macro): {f1:.4f}")
+
+    # Cohen's Kappa
+    kappa = cohen_kappa_score(y_true, y_pred)
+    print(f"Cohen's Kappa: {kappa:.4f}")
+
+    # Full classification report
+    print("\nClassification Report:")
+    report = classification_report(y_true, y_pred)
+    print(report)
