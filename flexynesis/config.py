58e529026455a418b6a29ab6ab83609efc9c92f1
borauyar <bora.uyar@mdc-berlin.de>
2023-03-24 21:07:22 +0100

    add svae tuning parameters

--- flexynesis/config.py
+++ flexynesis/config.py
@@ -9,6 +9,16 @@ space_model1 = [
     Integer(100, 200, name='epochs')
 ]
 
+space_model2 = [
+    Integer(16, 48, name='latent_dim'),
+    Integer(32, 128, name='hidden_dim'),
+    Integer(4, 16, name='supervisor_hidden_dim'),
+    Categorical([0.01, 0.001, 0.0001], name='lr'),
+    Integer(64, 128, name='batch_size'),
+    Integer(50, 100, name='epochs')
+]
+
 search_spaces = {
     'DirectPred': space_model1,
+    'SVAE': space_model2
 }
