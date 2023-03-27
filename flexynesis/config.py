8f4b1c01f5e1e0bee8511aa93395d11045cd24b8
borauyar <bora.uyar@mdc-berlin.de>
2023-03-27 15:50:44 +0200

    add SVAE params to config

--- flexynesis/config.py
+++ flexynesis/config.py
@@ -2,11 +2,11 @@
 from skopt.space import Integer, Categorical
 
 space_model1 = [
-    Integer(16, 128, name='latent_dim'),
-    Integer(32, 256, name='hidden_dim'),
+    Integer(16, 48, name='latent_dim'),
+    Integer(32, 128, name='hidden_dim'),
     Categorical([0.01, 0.001, 0.0001], name='lr'),
-    Integer(32, 64, name='batch_size'),
-    Integer(100, 200, name='epochs')
+    Integer(32, 128, name='batch_size'),
+    Integer(100, 500, name='epochs')
 ]
 
 space_model2 = [
