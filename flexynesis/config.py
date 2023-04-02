3c29187509ac209067563e5422cdfe9ab34a8aca
borauyar <bora.uyar@mdc-berlin.de>
2023-04-02 17:58:52 +0200

    convert hpo options to categorical

--- flexynesis/config.py
+++ flexynesis/config.py
@@ -2,29 +2,29 @@
 from skopt.space import Integer, Categorical
 
 space_model1 = [
-    Integer(16, 48, name='latent_dim'),
-    Integer(32, 128, name='hidden_dim'),
+    Categorical([16, 32, 48], name='latent_dim'),
+    Categorical([32, 64, 128], name='hidden_dim'),
     Categorical([0.01, 0.001, 0.0001], name='lr'),
-    Integer(32, 128, name='batch_size'),
-    Integer(100, 200, name='epochs')
+    Categorical([32, 64, 128], name='batch_size'),
+    Categorical([100, 150, 200], name='epochs')
 ]
 
 space_model2 = [
-    Integer(16, 48, name='latent_dim'),
-    Integer(32, 128, name='hidden_dim'),
-    Integer(4, 16, name='supervisor_hidden_dim'),
+    Categorical([16, 32, 48], name='latent_dim'),
+    Categorical([32, 64, 128], name='hidden_dim'),
+    Categorical([4, 8, 16], name='supervisor_hidden_dim'),
     Categorical([0.01, 0.001, 0.0001], name='lr'),
-    Integer(64, 128, name='batch_size'),
-    Integer(100, 200, name='epochs')
+    Categorical([32, 64, 128], name='batch_size'),
+    Categorical([100, 150, 200], name='epochs')
 ]
 
 space_model3 = [
-    Integer(16, 48, name='embedding_dim'),
-    Integer(32, 128, name='hidden_dim'),
-    Integer(4, 16, name='classifier_hidden_dim'),
+    Categorical([16, 32, 48], name='embedding_dim'),
+    Categorical([32, 64, 128], name='hidden_dim'),
+    Categorical([4, 8, 16], name='classifier_hidden_dim'),
     Categorical([0.01, 0.001, 0.0001], name='lr'),
-    Integer(64, 128, name='batch_size'),
-    Integer(100, 200, name='epochs')
+    Categorical([32, 64, 128], name='batch_size'),
+    Categorical([100, 150, 200], name='epochs')
 ]
 
 search_spaces = {
