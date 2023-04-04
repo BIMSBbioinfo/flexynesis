920ce45939e692c244caad86054db0641cad5f79
borauyar <bora.uyar@mdc-berlin.de>
2023-04-04 18:23:33 +0200

    change triplet encoder default search spaces

--- flexynesis/config.py
+++ flexynesis/config.py
@@ -19,12 +19,12 @@ space_model2 = [
 ]
 
 space_model3 = [
-    Categorical([16, 32, 48], name='embedding_dim'),
-    Categorical([32, 64, 128], name='hidden_dim'),
-    Categorical([4, 8, 16], name='classifier_hidden_dim'),
-    Categorical([0.01, 0.001, 0.0001], name='lr'),
-    Categorical([32, 64, 128], name='batch_size'),
-    Categorical([100, 150, 200], name='epochs')
+    Categorical([16, 48, 96], name='embedding_dim'),
+    Categorical([32, 128, 512], name='hidden_dim'),
+    Categorical([4, 16], name='classifier_hidden_dim'),
+    Categorical([0.001, 0.0001], name='lr'),
+    Categorical([64, 128], name='batch_size'),
+    Categorical([100, 200, 300, 400, 500], name='epochs')
 ]
 
 search_spaces = {
