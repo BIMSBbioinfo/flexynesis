be98fbcced4e1cd0ff40706de91a43fb6578887f
borauyar <bora.uyar@mdc-berlin.de>
2023-05-11 21:39:14 +0200

    use integer/real values where applicable for HPO

--- flexynesis/config.py
+++ flexynesis/config.py
@@ -1,34 +1,28 @@
 # config.py
 from skopt.space import Integer, Categorical, Real
 
-space_model1 = [
-    Categorical([16, 32, 48], name='latent_dim'),
-    Categorical([32, 64, 128], name='hidden_dim'),
-    Real(0.0001, 0.01, prior='log-uniform', name='lr'),
-    Categorical([32, 64, 128], name='batch_size'),
-    Categorical([100, 150, 200], name='epochs')
-]
-
-space_model2 = [
-    Categorical([16, 32, 48], name='latent_dim'),
-    Categorical([32, 64, 128], name='hidden_dim'),
-    Categorical([4, 8, 16], name='supervisor_hidden_dim'),
-    Real(0.0001, 0.01, prior='log-uniform', name='lr'),
-    Categorical([32, 64, 128], name='batch_size'),
-    Categorical([100, 150, 200], name='epochs')
-]
-
-space_model3 = [
-    Categorical([16, 48, 96], name='embedding_dim'),
-    Categorical([32, 128, 512], name='hidden_dim'),
-    Categorical([4, 16], name='classifier_hidden_dim'),
-    Real(0.0001, 0.01, prior='log-uniform', name='lr'),
-    Categorical([64, 128], name='batch_size'),
-    Categorical([100, 200, 300, 400, 500], name='epochs')
-]
-
 search_spaces = {
-    'DirectPred': space_model1,
-    'SVAE': space_model2,
-    'MultiTripletNetwork': space_model3
+    'DirectPred': [
+        Integer(16, 128, name='latent_dim'),
+        Integer(64, 512, name='hidden_dim'),
+        Real(0.0001, 0.01, prior='log-uniform', name='lr'),
+        Integer(32, 128, name='batch_size'),
+        Categorical([100, 200], name='epochs')
+    ], 
+    'SVAE': [
+        Integer(16, 128, name='latent_dim'),
+        Integer(64, 512, name='hidden_dim'),
+        Integer(8, 32, name='supervisor_hidden_dim'),
+        Real(0.0001, 0.01, prior='log-uniform', name='lr'),
+        Integer(32, 128, name='batch_size'),
+        Categorical([100, 200], name='epochs')
+    ],
+    'MultiTripletNetwork': [
+        Integer(16, 128, name='embedding_dim'),
+        Integer(64, 512, name='hidden_dim'),
+        Integer(8, 32, name='classifier_hidden_dim'),
+        Real(0.0001, 0.01, prior='log-uniform', name='lr'),
+        Integer(32, 128, name='batch_size'),
+        Categorical([100, 200], name='epochs')
+    ]
 }
