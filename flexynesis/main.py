3b6b7d1be481c1009809e1988840d5927b81e799
borauyar <bora.uyar@mdc-berlin.de>
2023-05-11 21:39:52 +0200

    use default settings for skopt.optimizer

--- flexynesis/main.py
+++ flexynesis/main.py
@@ -6,6 +6,8 @@ from pytorch_lightning.callbacks import TQDMProgressBar
 from pytorch_lightning.strategies import DDPStrategy
 from pytorch_lightning.loggers import TensorBoardLogger
 
+from tqdm import tqdm
+
 from skopt import Optimizer
 from skopt.utils import use_named_args
 from .config import search_spaces
@@ -33,21 +35,25 @@ class HyperparameterTuning:
         return val_loss    
     
     def perform_tuning(self):
-        opt = Optimizer(dimensions=self.space, n_initial_points=10, acq_func="EI", acq_optimizer="sampling")
+        opt = Optimizer(dimensions=self.space, n_initial_points=10, acq_func="gp_hedge", acq_optimizer="auto")
 
         best_loss = np.inf
         best_params = None
 
-        for i in range(self.n_iter):
-            suggested_params_list = opt.ask()
-            suggested_params_dict = {param.name: value for param, value in zip(self.space, suggested_params_list)}
-            loss = self.objective(suggested_params_dict)
-            opt.tell(suggested_params_list, loss)
+        with tqdm(total=self.n_iter, desc='Tuning Progress') as pbar:
+            for i in range(self.n_iter):
+                suggested_params_list = opt.ask()
+                suggested_params_dict = {param.name: value for param, value in zip(self.space, suggested_params_list)}
+                loss = self.objective(suggested_params_dict)
+                opt.tell(suggested_params_list, loss)
 
-            if loss < best_loss:
-                best_loss = loss
-                best_params = suggested_params_list
+                if loss < best_loss:
+                    best_loss = loss
+                    best_params = suggested_params_list
 
+                # Print result of each iteration
+                pbar.set_postfix({'Iteration': i+1, 'Best Loss': best_loss})
+                pbar.update(1)
         # Update the model-specific configuration with the best hyperparameters
         best_params_dict = {param.name: value for param, value in zip(self.space, best_params)}
         print("Building final model with best params:",best_params_dict)
@@ -56,3 +62,5 @@ class HyperparameterTuning:
         trainer = pl.Trainer(max_epochs=int(best_params_dict['epochs']))
         trainer.fit(model)
         return model, best_params
+    
+    
