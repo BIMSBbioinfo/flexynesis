e090cca0733b383f0bf5e24d496223d001760922
borauyar <bora.uyar@mdc-berlin.de>
2023-05-11 12:03:33 +0200

    use real values for learning rate

--- flexynesis/config.py
+++ flexynesis/config.py
@@ -1,10 +1,10 @@
 # config.py
-from skopt.space import Integer, Categorical
+from skopt.space import Integer, Categorical, Real
 
 space_model1 = [
     Categorical([16, 32, 48], name='latent_dim'),
     Categorical([32, 64, 128], name='hidden_dim'),
-    Categorical([0.01, 0.001, 0.0001], name='lr'),
+    Real(0.0001, 0.01, prior='log-uniform', name='lr'),
     Categorical([32, 64, 128], name='batch_size'),
     Categorical([100, 150, 200], name='epochs')
 ]
@@ -13,7 +13,7 @@ space_model2 = [
     Categorical([16, 32, 48], name='latent_dim'),
     Categorical([32, 64, 128], name='hidden_dim'),
     Categorical([4, 8, 16], name='supervisor_hidden_dim'),
-    Categorical([0.01, 0.001, 0.0001], name='lr'),
+    Real(0.0001, 0.01, prior='log-uniform', name='lr'),
     Categorical([32, 64, 128], name='batch_size'),
     Categorical([100, 150, 200], name='epochs')
 ]
@@ -22,7 +22,7 @@ space_model3 = [
     Categorical([16, 48, 96], name='embedding_dim'),
     Categorical([32, 128, 512], name='hidden_dim'),
     Categorical([4, 16], name='classifier_hidden_dim'),
-    Categorical([0.001, 0.0001], name='lr'),
+    Real(0.0001, 0.01, prior='log-uniform', name='lr'),
     Categorical([64, 128], name='batch_size'),
     Categorical([100, 200, 300, 400, 500], name='epochs')
 ]
