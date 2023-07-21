1fe177bbc8dc9cfcf3d1cc96f1b946ba8d71ea0f
borauyar <bora.uyar@mdc-berlin.de>
2023-07-21 13:24:40 +0200

    use common name for the embedding dim => latent_dim for all networks

--- flexynesis/config.py
+++ flexynesis/config.py
@@ -20,7 +20,7 @@ search_spaces = {
         Categorical(epochs, name='epochs')
     ],
     'MultiTripletNetwork': [
-        Integer(16, 128, name='embedding_dim'),
+        Integer(16, 128, name='latent_dim'),
         Integer(64, 512, name='hidden_dim'),
         Integer(8, 32, name='supervisor_hidden_dim'),
         Real(0.0001, 0.01, prior='log-uniform', name='lr'),
