7c30bc0df7c132b9171876b1df9838c2550f923b
Taras Savchyn <trsvchn@gmail.com>
2023-09-28 17:14:56 +0200

    Integrate to existing structure

--- flexynesis/models/__init__.py
+++ flexynesis/models/__init__.py
@@ -1,1 +1,5 @@
-__all__ = []
+from ..model_DirectPred import DirectPred
+from ..model_SVAE import supervised_vae as SupervisedVAE
+from ..model_TripletEncoder import MultiTripletNetwork
+
+__all__ = ["DirectPred", "SupervisedVAE", "MultiTripletNetwork"]
--- flexynesis/modules.py
+++ flexynesis/modules.py
@@ -1,4 +1,7 @@
 import torch
 from torch import nn
 
-__all__ = []
+from .models_shared import Encoder, Decoder, MLP, EmbeddingNetwork, Classifier
+from .model_TripletEncoder import MultiEmbeddingNetwork
+
+__all__ = ["Encoder", "Decoder", "MLP", "EmbeddingNetwork", "MultiEmbeddingNetwork", "Classifier"]
