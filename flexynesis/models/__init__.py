f2d3a938c7e5d5c0573078ec18d5fb240dfa801f
Taras Savchyn <trsvchn@gmail.com>
2023-12-18 10:24:33 +0100

    Finish DirectPredGCNN model

--- flexynesis/models/__init__.py
+++ flexynesis/models/__init__.py
@@ -1,6 +1,7 @@
 from .direct_pred import DirectPred
 from .direct_pred_cnn import DirectPredCNN
+from .direct_pred_gcnn import DirectPredGCNN
 from .supervised_vae import supervised_vae
 from .triplet_encoder import MultiTripletNetwork
 
-__all__ = ["DirectPred", "DirectPredCNN", "supervised_vae", "MultiTripletNetwork"]
+__all__ = ["DirectPred", "DirectPredCNN", "DirectPredGCNN", "supervised_vae", "MultiTripletNetwork"]
