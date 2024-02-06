c062a88b05438bae9382f8041c3a23f050ce2ff6
Ricardo Wurmus <rekado@elephly.net>
2024-02-06 12:02:34 +0100

    Specify commits for all used Guix channels.

--- channels.scm
+++ channels.scm
@@ -1,10 +1,44 @@
-(cons*
- (channel
-  (name 'guix-science-nonfree)
-  (url "https://github.com/guix-science/guix-science-nonfree.git")
-  (introduction
-   (make-channel-introduction
-    "58661b110325fd5d9b40e6f0177cc486a615817e"
-    (openpgp-fingerprint
-     "CA4F 8CF4 37D7 478F DA05  5FD4 4213 7701 1A37 8446"))))
- %default-channels)
+(list (channel
+       (name 'guix)
+       (url "https://git.savannah.gnu.org/git/guix.git")
+       (branch "master")
+       (commit
+        "2c5293afffa39d5d4aaec248a1973c7f62b1a0b4")
+       (introduction
+        (make-channel-introduction
+         "9edb3f66fd807b096b48283debdcddccfea34bad"
+         (openpgp-fingerprint
+          "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+      (channel
+       (name 'guix-science-nonfree)
+       (url "https://github.com/guix-science/guix-science-nonfree.git")
+       (branch "master")
+       (commit
+        "2492cda73c2091108fc43494338117e7d7dd92e5")
+       (introduction
+        (make-channel-introduction
+         "58661b110325fd5d9b40e6f0177cc486a615817e"
+         (openpgp-fingerprint
+          "CA4F 8CF4 37D7 478F DA05  5FD4 4213 7701 1A37 8446"))))
+      (channel
+       (name 'guix-past)
+       (url "https://gitlab.inria.fr/guix-hpc/guix-past")
+       (branch "master")
+       (commit
+        "d60a388678098e6de60ca1a65806903b5739da29")
+       (introduction
+        (make-channel-introduction
+         "0c119db2ea86a389769f4d2b9c6f5c41c027e336"
+         (openpgp-fingerprint
+          "3CE4 6455 8A84 FDC6 9DB4  0CFB 090B 1199 3D9A EBB5"))))
+      (channel
+       (name 'guix-science)
+       (url "https://github.com/guix-science/guix-science.git")
+       (branch "master")
+       (commit
+        "0b3d4a2f6d72c24f169bd5bf663f920ede5a8ae9")
+       (introduction
+        (make-channel-introduction
+         "b1fe5aaff3ab48e798a4cce02f0212bc91f423dc"
+         (openpgp-fingerprint
+          "CA4F 8CF4 37D7 478F DA05  5FD4 4213 7701 1A37 8446")))))
