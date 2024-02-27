c6ced046cad9dd233a5a793b3d9b5a5ab8df2d48
Bora Uyar <bora.uyar@mdc-berlin.de>
2024-02-27 14:24:47 +0100

    update direct pred to handle survival variables

--- flexynesis/models/direct_pred.py
+++ flexynesis/models/direct_pred.py
@@ -17,16 +17,21 @@ from captum.attr import IntegratedGradients
 
 from ..modules import *
 
-
-
 class DirectPred(pl.LightningModule):
-    def __init__(self, config, dataset, target_variables, batch_variables = None, val_size = 0.2, use_loss_weighting = True):
+    def __init__(self, config, dataset, target_variables, batch_variables = None, 
+                 surv_event_var = None, surv_time_var = None, val_size = 0.2, use_loss_weighting = True):
         super(DirectPred, self).__init__()
         self.config = config
         self.dataset = dataset
         self.target_variables = target_variables
+        # both surv event and time variables are assumed to be numerical variables
+        # we create only one survival variable for the pair (surv_time_var and surv_event_var)
+        if surv_event_var is not None and surv_time_var is not None:
+            self.target_variables = target_variables + [surv_event_var]
+            self.surv_event_var = surv_event_var
+            self.surv_time_var = surv_time_var
         self.batch_variables = batch_variables
-        self.variables = target_variables + batch_variables if batch_variables else target_variables
+        self.variables = self.target_variables + batch_variables if batch_variables else self.target_variables
         self.val_size = val_size
         self.dat_train, self.dat_val = self.prepare_data()
         self.feature_importances = {}
@@ -137,9 +142,15 @@ class DirectPred(pl.LightningModule):
         outputs = self.forward(x_list)
         losses = {}
         for var in self.variables:
-            y_hat = outputs[var]
-            y = y_dict[var]
-            loss = self.compute_loss(var, y, y_hat)
+            if var == self.surv_event_var:
+                durations = y_dict[self.surv_time_var]
+                events = y_dict[self.surv_event_var] 
+                risk_scores = outputs[var] #output of MLP
+                loss = cox_ph_loss(risk_scores, durations, events)
+            else:
+                y_hat = outputs[var]
+                y = y_dict[var]
+                loss = self.compute_loss(var, y, y_hat)
             losses[var] = loss
             
         total_loss = self.compute_total_loss(losses)
@@ -165,9 +176,15 @@ class DirectPred(pl.LightningModule):
         outputs = self.forward(x_list)
         losses = {}
         for var in self.variables:
-            y_hat = outputs[var]
-            y = y_dict[var]
-            loss = self.compute_loss(var, y, y_hat)
+            if var == self.surv_event_var:
+                durations = y_dict[self.surv_time_var]
+                events = y_dict[self.surv_event_var] 
+                risk_scores = outputs[var] #output of MLP
+                loss = cox_ph_loss(risk_scores, durations, events)
+            else:
+                y_hat = outputs[var]
+                y = y_dict[var]
+                loss = self.compute_loss(var, y, y_hat)
             losses[var] = loss
         total_loss = sum(losses.values())
         losses['val_loss'] = total_loss
