7e47be69582b731db178fca620b77a78c2ba5eda
Bora Uyar <bora.uyar@mdc-berlin.de>
2024-03-17 13:07:21 +0100

    export the class

--- flexynesis/models/__init__.py
+++ flexynesis/models/__init__.py
@@ -2,5 +2,6 @@ from .direct_pred import DirectPred
 from .direct_pred_gcnn import DirectPredGCNN
 from .supervised_vae import supervised_vae
 from .triplet_encoder import MultiTripletNetwork
+from .crossmodal_pred import CrossModalPred
 
-__all__ = ["DirectPred", "DirectPredGCNN", "supervised_vae", "MultiTripletNetwork"]
+__all__ = ["DirectPred", "DirectPredGCNN", "supervised_vae", "MultiTripletNetwork", "CrossModalPred"]
