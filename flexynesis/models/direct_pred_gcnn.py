d150f8d4ad8a491e50c33d47cb4b7b1a69a4485e
Bora Uyar <bora.uyar@mdc-berlin.de>
2024-05-14 19:28:23 +0200

    use batches in feature importance calculation => this doesnt solve captum's memory hungry calculations though

--- flexynesis/models/direct_pred_gcnn.py
+++ flexynesis/models/direct_pred_gcnn.py
@@ -11,6 +11,7 @@ import lightning as pl
 from torch_geometric.data import Data, Batch
 from torch_geometric.loader import DataLoader
 
+
 from captum.attr import IntegratedGradients
 
 from ..modules import MLP, cox_ph_loss, GNNs
@@ -138,8 +139,8 @@ class DirectPredGCNN(pl.LightningModule):
             total_loss = sum(losses.values())
         return total_loss
 
-    def training_step(self, train_batch, batch_idx):
-        dat, y_dict = train_batch
+    def training_step(self, train_batch):
+        (dat, y_dict), idx = train_batch
         layers = dat.keys()
         x_list = [dat[x] for x in layers]
 
@@ -163,8 +164,8 @@ class DirectPredGCNN(pl.LightningModule):
         self.log_dict(losses, on_step=False, on_epoch=True, prog_bar=True, batch_size=int(x_list[0].batch_size))
         return total_loss
 
-    def validation_step(self, val_batch, batch_idx):
-        dat, y_dict = val_batch
+    def validation_step(self, val_batch):
+        (dat, y_dict), idx = val_batch
         layers = dat.keys()
         x_list = [dat[x] for x in layers]
 
@@ -251,24 +252,18 @@ class DirectPredGCNN(pl.LightningModule):
             )
         return self.forward(inputs)[target_var]
 
-    def compute_feature_importance(self, dataset, target_var, steps=5):
+    def compute_feature_importance(self, dataset, target_var, steps=5, batch_size = 32):
         # find out the device the model was trained on.
         device = torch.device("cuda" if self.device_type == 'gpu' and torch.cuda.is_available() else 'cpu')
         self.to(device)
+
+        def bytes_to_gb(bytes):
+            return bytes / 1024 ** 2
         
         print("[INFO] Computing feature importance for variable:",target_var,"on device:",device)
-        
-        # Prepare inputs and baselines, moving them to the GPU
-        xs = [x.to(device) for x in dataset.dat.values()]
-        edge_indices = [dataset.feature_ann[k]["edge_index"].to(device) for k in dataset.dat.keys()]
 
-        inputs = tuple(xs)
-        baselines = tuple([torch.zeros_like(x) for x in inputs])
-
-        edge_index_arg = tuple(edge_indices)
-        additional_forward_args = (edge_index_arg, target_var)
-
-        internal_batch_size = inputs[0].shape[0]
+        # notice the DataLoader comes from torch_geometric.loader
+        dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
 
         ig = IntegratedGradients(self.model_forward)
 
@@ -278,56 +273,88 @@ class DirectPredGCNN(pl.LightningModule):
         else:
             num_class = len(np.unique(dataset.ann[target_var]))
 
-        # Compute the feature importance for each class
-        attributions = []
-        if num_class > 1:
-            for target_class in range(num_class):
-                attributions.append(
-                    ig.attribute(
-                        inputs,
-                        baselines,
-                        additional_forward_args=additional_forward_args,
-                        target=target_class,
-                        n_steps=steps,
-                        internal_batch_size=internal_batch_size,
-                    )
-                )
-        else:
-            attributions.append(
-                ig.attribute(
-                    inputs,
-                    baselines,
-                    additional_forward_args=additional_forward_args,
-                    n_steps=steps,
-                    internal_batch_size=internal_batch_size,
-                )
-            )
+        aggregated_attributions = [[] for _ in range(num_class)]
+        
+        print("Memory before batch processing: {:.3f} MB".format(bytes_to_gb(torch.cuda.max_memory_reserved())))
+        for batch in dataloader:
+            (dat, ydict), idx = batch
+            print("processing ",len(idx),"samples in batch")
+            subset = dataset[idx]
+            
+            # Prepare inputs and baselines, moving them to the GPU
+            xs = [x.to(device) for x in subset.dat.values()]
+            
+            print("Memory after moving xs to GPU: {:.3f} MB".format(bytes_to_gb(torch.cuda.max_memory_reserved())))
+            edge_indices = [subset.feature_ann[k]["edge_index"].to(device) for k in subset.dat.keys()]
+            print("Memory after moving edges to GPU: {:.3f} MB".format(bytes_to_gb(torch.cuda.max_memory_reserved())))
+            
+            inputs = tuple(xs)
+            baselines = tuple([torch.zeros_like(x) for x in inputs])
 
-        # Move computations back to the CPU for further processing
-        abs_attr = [[torch.abs(a).cpu() for a in attr_class] for attr_class in attributions]
+            edge_index_arg = tuple(edge_indices)
+            additional_forward_args = (edge_index_arg, target_var)
+            
+            del xs  # Explicitly delete if not needed anymore
+            torch.cuda.empty_cache()
+            print("Memory after clearing cache: {:.3f} MB".format(bytes_to_gb(torch.cuda.max_memory_reserved())))
+
+            
+            #internal_batch_size = inputs[0].shape[0]
+            #print("Internal batch size:",internal_batch_size)
+            if num_class == 1:
+                # returns a tuple of tensors (one per data modality)
+                attributions = ig.attribute(inputs, baselines,
+                                            additional_forward_args=additional_forward_args,
+                                            n_steps=steps) 
+                                            #internal_batch_size = internal_batch_size)  
+                aggregated_attributions[0].append(attributions)
+            else:
+                for target_class in range(num_class):
+                    attributions = ig.attribute(
+                            inputs,
+                            baselines,
+                            additional_forward_args=additional_forward_args,
+                            target=target_class,
+                            n_steps=steps) 
+                            #internal_batch_size = internal_batch_size)
+                    aggregated_attributions[target_class].append(attributions)
+
+        # For each target class and for each data modality/layer, concatenate attributions accross batches 
+        layers = list(dataset.dat.keys())
+        num_layers = len(layers)
+        processed_attributions = [] 
+        # Process each class
+        for class_idx in range(len(aggregated_attributions)):
+            class_attr = aggregated_attributions[class_idx]
+            layer_attributions = []
+            # Process each layer within the class
+            for layer_idx in range(num_layers):
+                # Extract all batch tensors for this layer across all batches for the current class
+                layer_tensors = [batch_attr[layer_idx] for batch_attr in class_attr]
+                # Concatenate tensors along the batch dimension
+                attr_concat = torch.cat(layer_tensors, dim=1)
+                layer_attributions.append(attr_concat)
+            processed_attributions.append(layer_attributions)
+
+        # compute absolute importance and move to cpu 
+        abs_attr = [[torch.abs(a).cpu() for a in attr_class] for attr_class in processed_attributions]
+        # average over samples 
         imp = [[a.mean(dim=1) for a in attr_class] for attr_class in abs_attr]
-
+        # move the model also back to cpu (if not already on cpu)
         self.to('cpu')
 
-        # Combine into a single data frame
+        # combine into a single data frame
         df_list = []
-        layers = list(dataset.dat.keys())
         for i in range(num_class):
             for j in range(len(layers)):
                 features = dataset.features[layers[j]]
-                importances = imp[i][j][0].detach().numpy()  # Safe to call .numpy() now, as tensors are on CPU
-                df_list.append(
-                    pd.DataFrame(
-                        {
-                            "target_variable": target_var,
-                            "target_class": i,
-                            "layer": layers[j],
-                            "name": features,
-                            "importance": importances,
-                        }
-                    )
-                )
+                # Ensure tensors are already on CPU before converting to numpy
+                importances = imp[i][j][0].detach().numpy()
+                df_list.append(pd.DataFrame({'target_variable': target_var, 
+                                             'target_class': i, 
+                                             'layer': layers[j], 
+                                             'name': features, 
+                                             'importance': importances}))    
         df_imp = pd.concat(df_list, ignore_index=True)
-
         # Save the computed scores in the model
         self.feature_importances[target_var] = df_imp
