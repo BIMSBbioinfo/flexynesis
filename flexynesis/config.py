68f4d90b62f6e19378d37bdfa6901c23d8f1d6b8
Bora Uyar <bora.uyar@mdc-berlin.de>
2024-07-06 13:17:55 +0200

    change hyperparameter arguments for gnn module

--- flexynesis/config.py
+++ flexynesis/config.py
@@ -1,7 +1,7 @@
 # config.py
 from skopt.space import Integer, Categorical, Real
 
-epochs = [200]
+epochs = [500]
 
 search_spaces = {
     'DirectPred': [
@@ -42,7 +42,8 @@ search_spaces = {
     ],
     'GNNEarly': [
         Integer(16, 128, name='latent_dim'),
-        Real(0.1, 0.25, name='hidden_dim_factor'), # relative size of the hidden_dim w.r.t input_dim 
+        Integer(4, 32, name='node_embedding_dim'), # node embedding dimensions
+        Integer(1, 4, name='num_convs'), # number of convolutional layers 
         Real(0.0001, 0.01, prior='log-uniform', name='lr'),
         Integer(8, 32, name='supervisor_hidden_dim'),
         Categorical(epochs, name='epochs'),
