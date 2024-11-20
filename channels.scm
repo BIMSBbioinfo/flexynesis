3409162ad3d88adc663260be30656ba7169f65b2
Ricardo Wurmus <rekado@elephly.net>
2024-11-20 21:42:47 +0100

    Update Guix channels.

--- channels.scm
+++ channels.scm
@@ -3,42 +3,29 @@
        (url "https://git.savannah.gnu.org/git/guix.git")
        (branch "master")
        (commit
-        "acb6cdc2990df2373707393d55da6827353862a8")
+        "da8afaa65fe30ae59e1bedbbb231490ad01c013c")
        (introduction
         (make-channel-introduction
          "9edb3f66fd807b096b48283debdcddccfea34bad"
          (openpgp-fingerprint
           "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
       (channel
-       (name 'guix-science-nonfree)
-       (url "https://github.com/guix-science/guix-science-nonfree.git")
-       (branch "master")
-       (commit
-        "5942e5a6370ce1a83872a824ee7056cad40cabd2")
-       (introduction
-        (make-channel-introduction
-         "58661b110325fd5d9b40e6f0177cc486a615817e"
-         (openpgp-fingerprint
-          "CA4F 8CF4 37D7 478F DA05  5FD4 4213 7701 1A37 8446"))))
-      (channel
-       (name 'guix-past)
-       (url "https://gitlab.inria.fr/guix-hpc/guix-past")
-       (branch "master")
-       (commit
-        "aab6148759476f0270bbe098919a978da45d7a2d")
-       (introduction
-        (make-channel-introduction
-         "0c119db2ea86a389769f4d2b9c6f5c41c027e336"
-         (openpgp-fingerprint
-          "3CE4 6455 8A84 FDC6 9DB4  0CFB 090B 1199 3D9A EBB5"))))
-      (channel
        (name 'guix-science)
-       (url "https://github.com/guix-science/guix-science.git")
-       (branch "master")
+       (url "https://codeberg.org/guix-science/guix-science.git")
        (commit
-        "12eba5aa0c5cbf357c476ef7872e0d562d054eff")
+        "e79f07d6ae81721e62d7cce78378f3ec49ff4efd")
        (introduction
         (make-channel-introduction
          "b1fe5aaff3ab48e798a4cce02f0212bc91f423dc"
+         (openpgp-fingerprint
+          "CA4F 8CF4 37D7 478F DA05  5FD4 4213 7701 1A37 8446"))))
+      (channel
+       (name 'guix-science-nonfree)
+       (url "https://codeberg.org/guix-science/guix-science-nonfree.git")
+       (commit
+        "446626ab1ca977b9278f431da8dde9ec8cf36457")
+       (introduction
+        (make-channel-introduction
+         "58661b110325fd5d9b40e6f0177cc486a615817e"
          (openpgp-fingerprint
           "CA4F 8CF4 37D7 478F DA05  5FD4 4213 7701 1A37 8446")))))
