3d13532e2ecdaed2693df80d47184cf8c43b57c1
Ricardo Wurmus <rekado@elephly.net>
2025-01-22 15:45:17 +0100

    Guix: Update flexynesis version.

--- guix.scm
+++ guix.scm
@@ -15,7 +15,7 @@
 
 (package
   (name "flexynesis")
-  (version "0.1.7")
+  (version "0.2.13")
   (source (git-checkout (url (dirname (current-filename)))))
   (build-system pyproject-build-system)
   (arguments
